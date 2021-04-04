d1ff7ee6bdb7c80631e63f1057f57e6003db1118
Justin Portell <72322829+jupo-ex-it@users.noreply.github.com>
2021-04-04 14:09:44 -0500

    completed main mission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// TODO: add code here
+window.addEventListener('load', function () {
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json')
+        .then(res => res.json())
+        .then(data => data.forEach(element => {
+            addAstronaut(element);
+        }));
+})
+
+function addAstronaut(element) {
+    var skills = element.skills.join(', ');
+    var container = document.getElementById('container');
+    container.insertAdjacentHTML('beforeend',
+        `<div class="astronaut">
+        <div class="bio">
+        <h3>${element.firstName} ${element.lastName}</h3>
+        <ul>
+            <li>Hours in space: ${element.hoursInSpace}</li>
+            <li>Active: ${element.active}</li>
+            <li>Skills: ${skills} </li>
+        </ul>
+        </div>
+        <img class="avatar" src="${element.picture}">
+    </div>`
+    )
+}
